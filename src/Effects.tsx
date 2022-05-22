2d8516a9d80b6b4b914ca910e3c924679019380f
Daniil <Smirnov_Danya_2001@mail.ru>
2022-05-22 13:50:09 +0300

    task-react-render-and-effects

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,24 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useState, useEffect } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [getSourId, setSoureId] = useState('first');
+    const [getNumForSourId, setNumForSoureId] = useState('-1');
+    const funcscribe = (param: any) => {
+        return setNumForSoureId(param);
+    };
+    useEffect(() => {
+        setSoureId(props.sourceId);
+        setNumForSoureId('-1');
+        subscribe(props.sourceId, funcscribe);
+        return () => {
+            unsubscribe(props.sourceId, funcscribe);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {getSourId}: {getNumForSourId}
+        </div>
+    );
 }
